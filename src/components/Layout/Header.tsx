8b78ec9e7744a5ec300730a29d8dbbb3ff9cb1ff
jlin3 <jesselinson@gmail.com>
2025-01-29 02:20:32 -0500

    Update header design to match mockup exactly

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -2,42 +2,82 @@
 
 import React from 'react'
 import Link from 'next/link'
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, Navbar, Form } from 'react-bootstrap'
+import { usePathname } from 'next/navigation'
+
+const DEVELOPER_TYPES = [
+  'Full-stack developer',
+  'Frontend developer',
+  'Backend developer',
+  'Mobile developer',
+  'Data scientist'
+]
 
 export default function Header() {
+  const pathname = usePathname()
+  const isSearchPage = pathname === '/find-developers'
+
   return (
-    <>
-      <Navbar bg="primary" variant="dark" className="py-2">
-        <Container>
-          <div className="d-flex justify-content-between align-items-center w-100">
-            <Link href="/" className="text-white text-decoration-none">
-              Developer Search
+    <header>
+      {/* Main Navigation */}
+      <Navbar bg="white" className="py-2">
+        <Container className="justify-content-end">
+          <div className="d-flex gap-4">
+            <Link href="/find-developers" className="text-secondary text-decoration-none">
+              Find Developers
             </Link>
-            <div className="d-flex gap-4">
-              <Link href="/find-developers" className="text-white text-decoration-none">
-                Find Developers
-              </Link>
-              <Link href="/resources" className="text-white text-decoration-none">
-                Resources
-              </Link>
-              <Link href="/login" className="text-white text-decoration-none">
+            <Link href="/resources" className="text-secondary text-decoration-none">
+              Resources
+            </Link>
+            <Link href="/sign-up" className="text-secondary text-decoration-none">
+              Sign Up
+            </Link>
+            <Link href="/login" className="text-decoration-none">
+              <button className="btn btn-primary btn-sm">
                 Login
-              </Link>
-            </div>
+              </button>
+            </Link>
           </div>
         </Container>
       </Navbar>
-      <Container fluid className="border-bottom bg-white">
-        <Container className="py-3">
-          <div className="d-flex align-items-center">
-            <Link href="/" className="text-decoration-none text-dark">
-              Home
-            </Link>
-            <span className="mx-2">›</span>
-            <span className="text-muted">Find Developers</span>
-          </div>
-        </Container>
-      </Container>
-    </>
+
+      {/* Search Section */}
+      {isSearchPage && (
+        <div className="bg-primary">
+          <Container>
+            {/* Breadcrumb */}
+            <div className="d-flex py-2">
+              <Link href="/" className="text-white text-decoration-none">
+                Home
+              </Link>
+              <span className="text-white mx-2">›</span>
+              <span className="text-white">Find Developers</span>
+            </div>
+            
+            {/* Search Controls */}
+            <div className="d-flex pb-3 gap-2">
+              <div className="dropdown">
+                <button 
+                  className="btn btn-light dropdown-toggle text-start" 
+                  type="button"
+                  style={{ minWidth: '200px' }}
+                >
+                  Full-stack developer
+                </button>
+              </div>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="United States"
+                style={{ maxWidth: '300px' }}
+              />
+              <button className="btn btn-secondary px-4">
+                Search
+              </button>
+            </div>
+          </Container>
+        </div>
+      )}
+    </header>
   )
 } 
